fa1db10f063cd9392b632f8e6ed7fc0b666437e7
AlexKazz <akazenoff@gmail.com>
2023-04-17 17:24:09 -0700

    add semi-functional key-based song fetching

--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,60 @@ import styles from "../styles/Home.module.css";
 const Home = () => {
   const [activeKeys, setActiveKeys] = useState({});
   const [previewUrls, setPreviewUrls] = useState({});
+  const [trackInfo, setTrackInfo] = useState({});
+  const [selectedKey, setSelectedKey] = useState("C");
+
   const audioRefs = useRef({});
 
-  useEffect(() => {
-    const fetchPreviewUrls = async () => {
-      const response = await axios.get("/api/spotify");
-      const accessToken = response.data.accessToken;
-      if (!accessToken) {
-        console.error("No access token available");
-        return;
-      }
+  const keyMapping = {
+    C: 0,
+    "C#": 1,
+    D: 2,
+    "D#": 3,
+    E: 4,
+    F: 5,
+    "F#": 6,
+    G: 7,
+    "G#": 8,
+    A: 9,
+    "A#": 10,
+    B: 11,
+  };
 
-      try {
-        const genresResponse = await axios.get(
-          "https://api.spotify.com/v1/browse/categories",
+  const fetchPreviewUrls = async () => {
+    const response = await axios.get("/api/spotify");
+    const accessToken = response.data.accessToken;
+    if (!accessToken) {
+      console.error("No access token available");
+      return;
+    }
+
+    ///
+    const selectedPitchClass = keyMapping[selectedKey];
+    ///
+
+    try {
+      const genresResponse = await axios.get(
+        "https://api.spotify.com/v1/browse/categories",
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      const genres = genresResponse.data.categories.items;
+
+      const randomGenre = (genres) => {
+        return genres[Math.floor(Math.random() * genres.length)];
+      };
+
+      const urls = {};
+
+      for (let i = 0; i < 10; i++) {
+        const genre = randomGenre(genres);
+        const playlistResponse = await axios.get(
+          `https://api.spotify.com/v1/browse/categories/${genre.id}/playlists?limit=1`,
           {
             headers: {
               Authorization: `Bearer ${accessToken}`,
@@ -26,51 +66,51 @@ const Home = () => {
           }
         );
 
-        const genres = genresResponse.data.categories.items;
+        const playlist = playlistResponse.data.playlists.items[0];
+        const tracksResponse = await axios.get(
+          `https://api.spotify.com/v1/playlists/${playlist.id}/tracks?limit=100`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
 
-        const randomGenre = (genres) => {
-          return genres[Math.floor(Math.random() * genres.length)];
+        const tracks = tracksResponse.data.items.map((item) => item.track);
+        const trackIds = tracks.map((track) => track.id);
+        const randomTrack = (tracks) => {
+          return tracks[Math.floor(Math.random() * tracks.length)];
         };
 
-        const urls = {};
+        const track = randomTrack(tracks);
+        urls[i] = track.preview_url;
+        trackInfo[i] = { artist: track.artists[0].name, name: track.name };
 
-        for (let i = 0; i < 10; i++) {
-          const genre = randomGenre(genres);
-          const playlistResponse = await axios.get(
-            `https://api.spotify.com/v1/browse/categories/${genre.id}/playlists?limit=1`,
-            {
-              headers: {
-                Authorization: `Bearer ${accessToken}`,
-              },
-            }
-          );
+        ///************* */
+        const audioFeaturesResponse = await axios.get(
+          `https://api.spotify.com/v1/audio-features?ids=${trackIds.join(",")}`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+        ///************* */
 
-          const playlist = playlistResponse.data.playlists.items[0];
-          const tracksResponse = await axios.get(
-            `https://api.spotify.com/v1/playlists/${playlist.id}/tracks?limit=100`,
-            {
-              headers: {
-                Authorization: `Bearer ${accessToken}`,
-              },
-            }
-          );
-
-          const tracks = tracksResponse.data.items.map((item) => item.track);
-          const randomTrack = (tracks) => {
-            return tracks[Math.floor(Math.random() * tracks.length)];
-          };
-
-          const track = randomTrack(tracks);
-          urls[i] = track.preview_url;
-        }
-
-        setPreviewUrls(urls);
-      } catch (error) {
-        console.error("Error fetching random songs:", error);
+        const audioFeatures = audioFeaturesResponse.data.audio_features;
+        const tracksInSelectedKey = tracks.filter(
+          (track, index) => audioFeatures[index].key === selectedPitchClass
+        );
       }
-    };
 
-    fetchPreviewUrls();
+      setPreviewUrls(urls);
+    } catch (error) {
+      console.error("Error fetching random songs:", error);
+    }
+  };
+
+  useEffect(() => {
+    // fetchPreviewUrls();
 
     const handleKeyDown = (e) => {
       if (e.key >= "0" && e.key <= "9") {
@@ -97,7 +137,7 @@ const Home = () => {
         if (audioRefs.current[e.key]) {
           setTimeout(() => {
             audioRefs.current[e.key].pause();
-          }, 1000);
+          }, 2000);
         }
       }
     };
@@ -109,32 +149,65 @@ const Home = () => {
       window.removeEventListener("keydown", handleKeyDown);
       window.removeEventListener("keyup", handleKeyUp);
     };
-  }, []);
+  }, [trackInfo]);
 
-  const renderButtons = () => {
-    const buttons = [];
-
-    for (let i = 0; i < 10; i++) {
-      buttons.push(
-        <button
-          key={i}
-          className={`${styles.button} ${activeKeys[i] ? styles.active : ""}`}
-        >
-          {i}
-          <audio
-            ref={(el) => (audioRefs.current[i] = el)}
-            src={previewUrls[i]}
-          />
-        </button>
-      );
-    }
-
-    return buttons;
-  };
+  useEffect(() => {
+    fetchPreviewUrls();
+  }, [selectedKey]);
 
   return (
     <div className={styles.container}>
-      <div className={styles.grid}>{renderButtons()}</div>
+      <div>
+        <title>Random Music Player</title>
+        <meta
+          name="description"
+          content="Play random 1-second music snippets"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </div>
+
+      <main className={styles.main}>
+        <select
+          className={styles.keySelect}
+          value={selectedKey}
+          onChange={(e) => setSelectedKey(e.target.value)}
+        >
+          <option value="C">C</option>
+          <option value="C#">C#</option>
+          <option value="D">D</option>
+          <option value="D#">D#</option>
+          <option value="E">E</option>
+          <option value="F">F</option>
+          <option value="F#">F#</option>
+          <option value="G">G</option>
+          <option value="G#">G#</option>
+          <option value="A">A</option>
+          <option value="A#">A#</option>
+          <option value="B">B</option>
+        </select>
+
+        <div className={styles.grid}>
+          {Object.keys(previewUrls).map((key) => (
+            <div key={key} className={styles.buttonContainer}>
+              <button
+                className={`${styles.keyButton} ${
+                  activeKeys[key] ? styles.active : ""
+                }`}
+              >
+                {key}
+                <audio
+                  ref={(el) => (audioRefs.current[key] = el)}
+                  src={previewUrls[key]}
+                />
+              </button>
+              <div className={styles.trackInfo}>
+                <div>{trackInfo[key]?.artist}</div>
+                <div>{trackInfo[key]?.name}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 };
