3bbc545d252b59d8097116e1fafdd4ec943fa3ef
AlexKazz <akazenoff@gmail.com>
2023-04-19 15:57:29 -0700

    improve randomness

--- utils/randomElement.js
+++ utils/randomElement.js
@@ -1,6 +1,16 @@
-// utils/randomElement.js
+// // utils/randomElement.js
+// const randomElement = (array) => {
+//   return array[Math.floor(Math.random() * array.length)];
+// };
+
+// export default randomElement;
+
+import { sample } from "lodash";
+
+// ...
+
 const randomElement = (array) => {
-  return array[Math.floor(Math.random() * array.length)];
+  return sample(array);
 };
 
 export default randomElement;
