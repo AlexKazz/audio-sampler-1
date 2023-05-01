79c14d96e8cc1aa9fa34af4abb9d48a2602f2b83
AlexKazz <akazenoff@gmail.com>
2023-05-01 13:44:12 -0700

    use handleSave() in button click

--- components/SaveModal.js
+++ components/SaveModal.js
@@ -37,11 +37,7 @@ const SaveModal = ({ showModal, setShowModal }) => {
           <button
             className="bg-custom-green text-white px-4 py-2 rounded mt-4 mr- hover:bg-green-400"
             onClick={() => {
-              localStorage.setItem(
-                inputValue,
-                JSON.stringify({ ...sampleState, timestamp: Date.now() })
-              );
-              setInputValue("");
+              handleSave();
               setShowModal(false);
             }}
           >
